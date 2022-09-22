54a2dc5d0c0eefb6eeb881e13d6de3335753cfd1
AlbertoBallesterosRivas <ballesterosrivasa@gmail.com>
2022-09-22 13:02:17 +0200

    Añadido formulario y conexión con la API con axios, el resultado de la consulta se muestra en consola

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import { useState } from "react";
+import axios from "axios";
 
-export default App
+const App = () => {
+  const [city, setCity] = useState("");
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const key = process.env.WEATHER_APP_API_KEY;
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+      )
+      .then(({ data }) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="city">City:</label>
+        <input
+          onChange={handleCityChange}
+          value={city}
+          type="text"
+          name="city"
+        />
+        <button type="submit">Find</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
